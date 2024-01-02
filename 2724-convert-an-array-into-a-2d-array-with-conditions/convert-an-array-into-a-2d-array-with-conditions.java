0d4367283620ab71af63cd460f375466b5bf71eb
gunaseelan03 <122155683+gunaseelan03@users.noreply.github.com>
2024-01-02 10:34:39 +0530

    Time: 4 ms (55.08%) | Memory: 44.7 MB (35.81%) - LeetSync

---
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        int freq[] = new int[nums.length+1];
+        List<List<Integer>>ans = new ArrayList<>();
+        for(int n:nums){
+            if(freq[n]>=ans.size())
+                ans.add(new ArrayList<>());
+            ans.get(freq[n]).add(n);
+            freq[n]++;
+        }
+        return ans;
+    }
+}
